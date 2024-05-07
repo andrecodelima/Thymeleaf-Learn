3a6ebb408f793a545dc5b54476ac12f4d973914a
andrecodelima <andrelimag89@gmail.com>
2024-05-07 16:45:10 -0300

    upload project

---
+++ projeto_thymeleaf-andre/src/main/java/com/udemy/mballen/ProjetoThymeleafAndreApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.mballen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoThymeleafAndreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoThymeleafAndreApplication.class, args);
+	}
+
+}
